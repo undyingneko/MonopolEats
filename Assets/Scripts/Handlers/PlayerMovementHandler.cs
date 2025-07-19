07cb66a5a0e2d5777d456d4b294fc7c14f26c2dd
fenryshu <dongoctram1404@gmail.com>
2025-07-19 10:19:47 +0200

    test another player

--- Assets/Scripts/Handlers/PlayerMovementHandler.cs
+++ Assets/Scripts/Handlers/PlayerMovementHandler.cs
@@ -1,20 +1,28 @@
-using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
 namespace SteamLobbyTutorial
 {
+    [RequireComponent(typeof(Rigidbody2D))]
     public class PlayerMovementHandler : NetworkBehaviour
     {
-        void Update()
+        public float moveSpeed = 5f;
+        private Rigidbody2D rb;
+
+        void Start()
         {
-            if (isLocalPlayer)
-            {
-                float h = Input.GetAxis("Horizontal");
-                float v = Input.GetAxis("Vertical");
-                Vector3 playerMovement = new Vector3(h * 0.25f, v * 0.25f, 0);
-                transform.position = transform.position + playerMovement;
-            }
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        void FixedUpdate()
+        {
+            if (!isLocalPlayer) return;
+
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            Vector2 movement = new Vector2(h, v) * moveSpeed;
+            rb.velocity = movement;
         }
     }
 }
